12af87f05134892e89cdb917f6cf982078f39633
Ubuntu <ubuntu@ip-172-31-47-219.eu-west-1.compute.internal>
2016-09-26 10:08:45 +0000

    initial commit

---
+++ api/controllers/MailController.js
@@ -0,0 +1,31 @@
+/**
+ * MailController
+ *
+ * @description :: Server-side logic for managing mails
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `MailController.sendOne()`
+   */
+  sendOne: function (req, res) {
+    return res.json({
+      todo: 'sendOne() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `MailController.sendBulk()`
+   */
+  sendBulk: function (req, res) {
+    return res.json({
+      todo: 'sendBulk() is not implemented yet!'
+    });
+  }
+};
+
