b715d7dc431c24e8cc29ec4d22f1033a6f58c7fd
Eliyahu Akinitazov <ilusha1991@gmail.com>
2016-10-05 16:41:45 +0300

    fixed some things

--- api/services/MailerService.js
+++ api/services/MailerService.js
@@ -1,13 +1,16 @@
+"use strict"; 
 
 var nodemailer = require('nodemailer');
 var sesTransport = require('nodemailer-ses-transport');
+var creds = require('../../../mail-cred.json');
 
 var transport = nodemailer.createTransport(sesTransport({
-    accessKeyId: "AKIAJK2NTZ7HD3SMFKAA",
+    accessKeyId: creds.id,
     region: "eu-west-1",
-    secretAccessKey: "Xyso4mMzjKVbDS4mQgIxxbTRKoDGm6NHyPc08TDS",
+    secretAccessKey: creds.key,
     rateLimit: 10 // do not send more than 5 messages in a second
 }));
+
 transport.verify(function(error, success) {
    if (error) {
         console.log(error);
@@ -15,11 +18,10 @@ transport.verify(function(error, success) {
         console.log('Server is ready to take our messages');
    }
 });
-
-
-
+"use strict";
 
 module.exports = {
+
     sendMail: function (arr,cb) {
        	if(!arr)
 	{
@@ -28,9 +30,9 @@ module.exports = {
 	for(let i=0 ; i<arr.length ; i++){
 	     var mailOptions = {
                 to: arr[i].to,
-                from: 'test@mail.getascola.com',
+                from: arr[i].from,
                 subject: arr[i].subject,    
-                html: '<b>Hello</b>'
+                html: arr[i].html
             };
             transport.sendMail(mailOptions, function(err, info) {
                  //re-render contact page with message
@@ -44,4 +46,4 @@ module.exports = {
         cb(null ,"emails added to mailer");  
     }
 	
-}
+};
---
+++ ssh.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -i ec2-mail-server.pem ubuntu@ec2-54-154-142-234.eu-west-1.compute.amazonaws.com
