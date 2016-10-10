d2f21fc1f15e17e914bdfd8fed17a677c6dbf757
Eliyahu Akinitazov <ilusha1991@gmail.com>
2016-10-10 14:35:16 +0300

    added responce

--- api/controllers/MailController.js
+++ api/controllers/MailController.js
@@ -5,18 +5,18 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-var nodemailer = require('nodemailer');
-var sesTransport = require('nodemailer-ses-transport');
+ var nodemailer = require('nodemailer');
+ var sesTransport = require('nodemailer-ses-transport');
 
 
-module.exports = {
-	
+ module.exports = {
+
 
 
   /**
    * `MailController.sendOne()`
    */
-  sendOne: function (req, res) {
+   sendOne: function (req, res) {
 
     return res.json({
       todo: 'sendOne() is not implemented yet!'
@@ -27,19 +27,17 @@ module.exports = {
   /**
    * `MailController.sendBulk()`
    */
-    sendBulk: function (req, res) {
- 	  
-	MailerService.sendMail(req.body , function(err,data){
-                if (err) {
-          	       console.log(err);
-		       return res.serverError();
-        	}else{
-	        	console.log(data);
-	        	return res.ok();
-         	}  
-     
-           
-	})
-    }
+   sendBulk: function (req, res) {
+
+    MailerService.sendMail(req.body , function(err,data){
+      if (err) {
+        console.log(err);
+        return res.serverError();
+      }else{
+        console.log(data);
+        return res.ok();
+      }  
+    });
+  }
 };
 
